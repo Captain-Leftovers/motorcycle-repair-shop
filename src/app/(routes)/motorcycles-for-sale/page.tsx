601fc923e58eb0e4ded86ecf3ac64c9c56b40837
Dobromir Stefanov Ivanov <lalilulelo.mg@gmail.com>
2023-11-04 16:11:03 +1100

    before next 14 update

--- src/app/(routes)/motorcycles-for-sale/page.tsx
+++ src/app/(routes)/motorcycles-for-sale/page.tsx
@@ -1,11 +1,26 @@
-
 import { Button } from '@/components/ui/Button'
 import MainContainer from '@/components/ui/MainContainer'
+import { MotoItemWithImagesType } from '@/types'
+import axios from 'axios'
 
-export default function MotorcyclesForSale() {
+export default async function MotorcyclesForSale() {
+	console.log('This is my moto for sale')
+
+	try {
+		const moto = await axios.get(
+			process.env.NEXT_CMS_API_GET_MOTO!
+		)
+		const motoData: MotoItemWithImagesType[] = moto.data
+	} catch (error) {
+		console.log(error)
+	
+	}
+
 	return (
 		<MainContainer>
-			<h1 className="text-amber-800 text-3xl">This is my Moto For Sale</h1>
+			<h1 className="text-amber-800 text-3xl">
+				This is my Moto For Sale
+			</h1>
 			<Button className="">Click me</Button>
 		</MainContainer>
 	)
--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,26 @@ export type ApiImageType = {
     description: string
     price: string
 }
+
+export type MotoItem = {
+    id: string;
+    make: string;
+    model: string | null;
+    description: string | null;
+    price: number | null;
+    coverUrl: string | null;
+    featured: boolean;
+    sold: boolean;
+    onHold: boolean;
+    addedByUserId: string;
+    updatedAt: Date;
+    createdAt: Date;
+}
+
+export type MotoItemImages = {
+    id: string;
+    motoItemId: string;
+    url: string;
+}
+
+export type MotoItemWithImagesType = MotoItem & {images: MotoItemImages[]}
